11030293a8fac3fb5307118b4449aa73bf0584c4
davidschulte <davidsiriusschulte@gmail.com>
2024-11-10 21:08:28 +0100

    Use main folder readme for PyPi readme

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("src/hfselect/README.md", "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
@@ -20,7 +20,17 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=["bson >= 0.5.10"],
+    install_requires=[
+        "bson >= 0.5.10"
+        "auto_mix_prep == 0.2.0",
+        "datasets == 3.0.2",
+        "huggingface_hub == 0.26.1",
+        "numba == 0.57.1"
+        "numpy == 1.24.3",
+        "torch == 2.1.2",
+        "tqdm == 4.66.5",
+        "transformers == 4.32.1"
+    ],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
