d30e875766a785215919994aa13337aa27eb5770
davidschulte <davidsiriusschulte@gmail.com>
2025-01-18 13:22:08 +0100

    Add ESM training to __init__

--- src/hfselect/__init__.py
+++ src/hfselect/__init__.py
@@ -1,7 +1,7 @@
 from .setup_logger import logger
 from .esm_logme import *
 from .ESM import *
-# from .ESMConfig import *
-from .embedding_dataset import EmbeddingDataset
+from .embedding_dataset import EmbeddingDataset, create_embedding_dataset
 from .dataset import Dataset
 from .utils import *
+from .trainers import ESMTrainer
